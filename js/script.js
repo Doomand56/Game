750cf8ba7f5c95de733d604acb20de77ff2513b2
Doomand56 <doomand56@gmail.com>
2022-04-07 22:05:58 +0300

    version 0.3

--- js/script.js
+++ js/script.js
@@ -4,12 +4,25 @@ let input = document.querySelector('.inp');
 
 let outComp = document.querySelector('.comp-num');
 let outMy = document.querySelector('.my-num');
-// outComp = '';
-// outMy = '';
 
-buttonOne.onclick = (min, max) => {
-    outComp.innerHTML = 'good!';
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-buttonTwo.onclick = (min, max) => {
+buttonOne.onclick = () => {
+    outComp.innerHTML = getRandomIntInclusive(0, 10);
+}
+buttonTwo.onclick = () => {
     outMy.innerHTML = input.value;
+}
+
+console.log(outComp);
+console.log(outMy);
+
+
+document.querySelector('.btn-3').onclick = () => {
+    if (outComp.value === outMy.value) {
+        console.log('Good!');
+    }
 }
