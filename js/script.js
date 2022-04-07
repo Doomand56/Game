e779d0256e2bee306ac3ece0dcb3aca52e53e629
Doomand56 <doomand56@gmail.com>
2022-04-07 13:04:38 +0300

    version 0.2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Good!');
+let buttonOne = document.querySelector('.btn-1');
+let buttonTwo = document.querySelector('.btn-2');
+let input = document.querySelector('.inp');
+
+let outComp = document.querySelector('.comp-num');
+let outMy = document.querySelector('.my-num');
+// outComp = '';
+// outMy = '';
+
+buttonOne.onclick = (min, max) => {
+    outComp.innerHTML = 'good!';
+}
+buttonTwo.onclick = (min, max) => {
+    outMy.innerHTML = input.value;
+}
